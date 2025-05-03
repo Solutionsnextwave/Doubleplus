546f3a1e30853aa821f0ae97ec273827dc296f64
Solutionsnextwave <gogulan@solutionsnext.in>
2025-05-04 00:54:00 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-# streamlit_app main file
-print('Hello from Streamlit App')
+# streamlit_app.py
+import streamlit as st
+
+st.set_page_config(page_title="Double Plus | Replenishment Tool", layout="wide")
+st.title("📦 Double Plus Replenishment Tool")
+st.sidebar.success("Select a page above.")
